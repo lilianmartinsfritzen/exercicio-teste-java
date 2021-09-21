3c72b04435bde7df451faa1d1cbe71315f846d0f
Lilian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2021-09-21 15:11:00 -0300

    Construtor: Deve criar o objeto com os dados corretos quando os dados forem válidos.

--- src/tests/entities/FinancingTests.java
+++ src/tests/entities/FinancingTests.java
@@ -1,5 +1,23 @@
 package tests.entities;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Financing;
+
 public class FinancingTests {
-	
+				// <AÇÃO> should <EFEITO> [when <CENÁRIO>]
+	@Test		// Criar novo financiamento quando for elegível
+	public void createShouldNewFundingWhenEligible() {
+		
+		// Arrange: instancie os objetos necessários
+
+		// Act: execute as ações necessárias
+		Financing financ = new Financing(100000.0, 5000.0, 32);
+		
+		// Assert: declare o que deveria acontecer (resultado esperado)
+		Assertions.assertEquals(100000.0, financ.getTotalAmount());
+		Assertions.assertEquals(5000.0, financ.getIncome());
+		Assertions.assertEquals(32, financ.getMonths());
+	}
 }
